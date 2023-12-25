7affbd8feb8d52003a57a42eb568b738841afa22
Thong Nguyen <thongnt.it@gmail.com>
2023-12-25 14:38:28 +0700

    feat: Re upload helloworld example with dart 3. (#23)

--- flutter/helloword/android/app/src/main/kotlin/com/example/helloword/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.helloword
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ flutter/helloworld/untitled/android/app/src/main/kotlin/thongnt/dev/another_helloworld/MainActivity.kt
@@ -0,0 +1,6 @@
+package thongnt.dev.another_helloworld
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
