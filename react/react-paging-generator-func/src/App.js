b4469f5c904c532468df9a3b38899dda5ce1cd02
Thong Nguyen <thongnt.it@gmail.com>
2024-11-07 01:47:46 +0700

    feat(React): add generator function pagination example (#33)

---
+++ react/react-paging-generator-func/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import PaginationTable from './Table';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PaginationTable />
+      </header>
+    </div>
+  );
+}
+
+export default App;
